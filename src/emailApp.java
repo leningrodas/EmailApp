544ef6f76013d8e056abcef020517cb08827d50c
lening rodas <lening.a.rodas@gmail.com>
2021-01-06 11:11:27 -0600

    update

--- src/emailApp.java
+++ src/emailApp.java
@@ -1,9 +1,9 @@
 public class emailApp {
-    public static  void main(String[] args) {
-        email em1 = new email("Lening", "roda");
-        System.out.println(em1.showInfo());
-
-    }
+//    public static  void main(String[] args) {
+//        email em1 = new email("Lening", "roda");
+//        System.out.println(em1.showInfo());
+//
+//    }
 
 
 
